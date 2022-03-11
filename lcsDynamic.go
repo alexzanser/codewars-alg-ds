486624faec24f03e7babf2654dbb9d950aaafd2d
tolkachev <alexzanser@mail.ru>
2022-03-11 21:25:07 +0300

    1

--- lcsDynamic.go
+++ lcsDynamic.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
 
 func fillDynMatrix(x, y []byte) [][]int {
 	L := make([][]int, len(x))
@@ -8,16 +18,36 @@ func fillDynMatrix(x, y []byte) [][]int {
 		L[i] = make([]int, len(y))
 	}
 
-	for _, line := range L {
-		fmt.Println(line)
+	for i:= 1; i < len(x); i++ {
+		for j:=1; j < len(y); j++ {
+			if x[i] == y[j]{
+				L[i][j] = L[i -1][j -1] + 1
+			} else {
+				L[i][j] = Max(Max(L[i - 1][j], L[i][j - 1]), L[i -1][j -1])
+			}
+		}
 	}
-
 	return L
+}
+
+func lcsDyn(L [][]int, x , y []byte) string {
+	res := ""
+	k := -1
+	for i:= 0; i < len(L); i++ {
+		for j:= 0; j < len(L[0]);j++ {
+			if L[i][j] > k {
+				res += string(x[i])
+				k += 1
+			}
+		}
+	}
+	return res
 }
 
 func main() {
 	x := []byte("abc")
 	y := []byte("abcdef")
 	
-	fillDynMatrix(x, y)
+	L := fillDynMatrix(x, y)
+	fmt.Println(lcsDyn(L, x, y))
 }
